52eb12376386be33e40b460a8e24bd5b0341b841
Alishba <alishba.soomro@student.ambassador.com>
2024-02-24 20:29:54 +0500

    add all changes

--- src/Components/Portfolio.jsx
+++ src/Components/Portfolio.jsx
@@ -19,7 +19,7 @@ import React from "react";
  * Need an image? Check out https://unsplash.com to download a photo you
  * freely use on your site.
  */
-import image from "../design-desk.jpeg";
+import image from "../office.jpeg";
 
 const imageAltText = "desktop with book and coffee on table";
 
